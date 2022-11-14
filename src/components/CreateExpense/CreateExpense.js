9e8f60b60fd7a8f5089068a16a39c5bafc7cf37a
TomasCalabria <totocalabria.tc@gmail.com>
2022-11-14 18:41:02 -0300

    .

--- src/components/CreateExpense/CreateExpense.js
+++ src/components/CreateExpense/CreateExpense.js
@@ -19,7 +19,7 @@ const submitGasto=(e)=>{
 
   e.preventDefault()
   console.log({title:title.current.value,amount:monto.current.value,type:tipo.current.value,category:categoria.current.value,creator:user.userInfo.username,participants:friendsToShare})
-axios.post(`${prod_url}`,{title:title.current.value,amount:monto.current.value,type:tipo.current.value,category:(categoria.current.value.charAt(0).toUpperCase()+categoria.current.value.slice(1).toLowerCase()),creator:user.userInfo.username,participants:friendsToShare})
+axios.post(`${local_url}`,{title:title.current.value,amount:monto.current.value,type:tipo.current.value,category:(categoria.current.value.charAt(0).toUpperCase()+categoria.current.value.slice(1).toLowerCase()),creator:user.userInfo.username,participants:friendsToShare})
 .then((res)=>{if(res.status===201){console.log(`Data sent! ${res.data}`,setStatus(res.status))}}
 )
 
